f5077ce9f92feffff660e7ea48affa625ab8e143
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-07-13 22:03:26 +0300

    Tehtävä 3.1 puhelinluettelo backend step 1

--- puhelinluettelo/index.js
+++ puhelinluettelo/index.js
@@ -2,10 +2,35 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "0400-123123",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "0400-123123",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "0400-123123",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "0400-123123",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello Worldss!</h1>");
 });
-
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
 const PORT = 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
